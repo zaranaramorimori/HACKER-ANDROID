454ba0d2e1d30993846234b89147f3bb23af9df6
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-03-03 11:21:39 +0900

    :wrench: Add build-logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.teamzzong.hacker.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    compileOnly(libs.android.build)
+    compileOnly(libs.kotlin.gradle)
+}
+
+gradlePlugin {
+    plugins {
+        create("android-application") {
+            id = "com.teamzzong.hacker.application"
+            implementationClass = "com.teamzzong.hacker.plugin.AndroidApplicationPlugin"
+        }
+        create("android-feature") {
+            id = "com.teamzzong.hacker.feature"
+            implementationClass = "com.teamzzong.hacker.plugin.AndroidFeaturePlugin"
+        }
+        create("android-kotlin") {
+            id = "com.teamzzong.hacker.kotlin"
+            implementationClass = "com.teamzzong.hacker.plugin.AndroidKotlinPlugin"
+        }
+        create("android-hilt") {
+            id = "com.teamzzong.hacker.hilt"
+            implementationClass = "com.teamzzong.hacker.plugin.AndroidHiltPlugin"
+        }
+        create("kotlin-serialization") {
+            id = "com.teamzzong.hacker.serialization"
+            implementationClass = "com.teamzzong.hacker.plugin.KotlinSerializationPlugin"
+        }
+    }
+}
